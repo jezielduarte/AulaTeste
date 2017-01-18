47043473e7ef6abe019599695a6e3a5236e85a96
jezielduarte <jezieloliveiravba@hotmail.com>
2017-01-18 13:41:30 -0300

    Repositorio do Fornecedor

--- DAL/Repositorios/RepositorioBase.cs
+++ DAL/Repositorios/RepositorioBase.cs
@@ -6,44 +6,41 @@ using System.Linq.Expressions;
 
 namespace DAL.Repositorios
 {
-    public class RepositorioBase
+    public class RepositorioBase<TEntity> where TEntity : class
     {
-        public class RepositoryBase<TEntity> where TEntity : class
+        protected Contexto Db = new Contexto();
+
+        public void Add(TEntity obj)
         {
-            protected Contexto Db = new Contexto();
+            Db.Set<TEntity>().Add(obj);
+            Db.SaveChanges();
+        }
 
-            public void Add(TEntity obj)
-            {
-                Db.Set<TEntity>().Add(obj);
-                Db.SaveChanges();
-            }
+        public TEntity GetById(int id)
+        {
+            return Db.Set<TEntity>().Find(id);
+        }
 
-            public TEntity GetById(int id)
-            {
-                return Db.Set<TEntity>().Find(id);
-            }
+        public IEnumerable<TEntity> GetAll()
+        {
+            return Db.Set<TEntity>().ToList();
+        }
 
-            public IEnumerable<TEntity> GetAll()
-            {
-                return Db.Set<TEntity>().ToList();
-            }
+        public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+        {
+            return Db.Set<TEntity>().Where(predicate).ToList();
+        }
 
-            public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
-            {
-                return Db.Set<TEntity>().Where(predicate).ToList();
-            }
+        public void Update(TEntity obj)
+        {
+            Db.Entry(obj).State = EntityState.Modified;
+            Db.SaveChanges();
+        }
 
-            public void Update(TEntity obj)
-            {
-                Db.Entry(obj).State = EntityState.Modified;
-                Db.SaveChanges();
-            }
-
-            public void Remove(TEntity obj)
-            {
-                Db.Set<TEntity>().Remove(obj);
-                Db.SaveChanges();
-            }
+        public void Remove(TEntity obj)
+        {
+            Db.Set<TEntity>().Remove(obj);
+            Db.SaveChanges();
         }
     }
 }
---
+++ DAL/Repositorios/RepositorioFornecedor.cs
@@ -0,0 +1,9 @@
+﻿using Dominio.Entidades;
+
+namespace DAL.Repositorios
+{
+    public class RepositorioFornecedor : RepositorioBase<Fornecedor>
+    {
+
+    }
+}
